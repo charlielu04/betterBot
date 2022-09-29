1a6bb32daf1d03318952e7dafa882a68799065b0
BrandonDu <82884213+BrandonDu@users.noreply.github.com>
2022-09-29 14:05:03 -0400

    test

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Charlie! You should be able to see this.')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
