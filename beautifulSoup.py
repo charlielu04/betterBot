9d0e55e4d48620a77f0b0a3d22c52365c44ea06f
BrandonDu <82884213+BrandonDu@users.noreply.github.com>
2022-10-14 17:52:53 -0400

    Found all teams and bets
    TODO: Sort out soccer and make hashmaps for each game

--- beautifulSoup.py
+++ beautifulSoup.py
@@ -8,21 +8,36 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-path = 'C:\Program Files (x86)\chromedriver.exe'
-url = 'https://sportsbook-nj.tipico.us/live'
 
-options = Options()
-options.headless = True
-options.add_argument("--start-maximized")
+def get_html(path, url):
+    options = Options()
+    options.headless = True
+    options.add_argument("--start-maximized")
 
-driver = webdriver.Chrome(path, options=options)
+    driver = webdriver.Chrome(path, options=options)
 
-# result = requests.get(url)
-# doc = BeautifulSoup(result.text, "html.parser")
-# print(doc.prettify())
+    # result = requests.get(url)
+    # doc = BeautifulSoup(result.text, "html.parser")
+    # print(doc.prettify())
 
-driver.get(url)
-time.sleep(3)
-page_source = driver.page_source
-soup = BeautifulSoup(page_source, 'html')
-print(soup.prettify())
+    driver.get(url)
+    time.sleep(3)
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'html')
+    # print(soup.prettify())
+
+    bets = soup.find_all("span", {"class": "sc-giYglK sc-kqnjJL fMUpmO eeEWlm"})
+    teams = soup.find_all("span", {"class": "sc-giYglK fMUpmO sc-bBHHxi gRkMpx"})
+    teams2 = soup.find_all("span", {"class": "sc-giYglK fMUpmO sc-bBHHxi emjYgw"})
+
+    for team in teams:
+        print(team.contents)
+    for team in teams2:
+        print(team.contents)
+    for bet in bets:
+        print(bet.contents)
+
+
+
+url = 'https://sportsbook-nj.tipico.us/today'
+get_html('/usr/local/bin/chromedriver', url)
